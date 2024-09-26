6d729bcba3445fd6ba1f85b17eb091e0b9094f1f
Зінченко Вікторія <147725988+migadela@users.noreply.github.com>
2024-09-26 19:32:32 +0300

    Update 041t5.rs

--- src/041t5.rs
+++ src/041t5.rs
@@ -2,6 +2,6 @@
 // Fix errors and panics to make it work
 fn main() {
    let v1 = 251_u8 + 8;
-   let v2 = i8::checked_add(251, 8).unwrap();
+   let v2 = u8::checked_add(251, 8).unwrap(u8::MAX);
    println!("{},{}",v1,v2);
 }
