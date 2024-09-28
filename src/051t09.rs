d0129576cc4e815cc2422d9d20247faec1365290
Зінченко Вікторія <147725988+migadela@users.noreply.github.com>
2024-09-28 14:21:57 +0200

    Create 051t09.rs

---
+++ src/051t09.rs
@@ -0,0 +1,9 @@
+
+fn main() {
+   let t = (String::from("hello"), String::from("world"));
+
+    // Fill the blanks
+    let (__, __) = __;
+
+    println!("{:?}, {:?}, {:?}", s1, s2, t); // -> "hello", "world", ("hello", "world")
+}
