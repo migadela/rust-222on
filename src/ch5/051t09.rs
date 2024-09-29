2df6e2d3b562044e3c3cf9f1d005ad0800686be3
Зінченко Вікторія <147725988+migadela@users.noreply.github.com>
2024-09-29 22:27:12 +0300

    Update and rename src/051t09.rs to src/ch5/051t09.rs

--- src/ch5/051t09.rs
+++ src/ch5/051t09.rs
@@ -3,7 +3,7 @@ fn main() {
    let t = (String::from("hello"), String::from("world"));
 
     // Fill the blanks
-    let (__, __) = __;
+    let (s1, s2) = (&t.0, &t.1);
 
     println!("{:?}, {:?}, {:?}", s1, s2, t); // -> "hello", "world", ("hello", "world")
 }
