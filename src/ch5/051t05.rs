2ca3cfb57df2687d22e0ef23e33f708142c99368
Зінченко Вікторія <147725988+migadela@users.noreply.github.com>
2024-09-29 21:52:45 +0300

    Update and rename src/051t05.rs to src/ch5/051t05.rs
    "hello", є типом &str. Цей тип реалізує трейт Copy

--- src/ch5/051t05.rs
+++ src/ch5/051t05.rs
@@ -1,6 +1,6 @@
 // Don't use clone ,use copy instead
 fn main() {
-    let x = (1, 2, (), "hello".to_string());
-    let y = x.clone();
+    let x = (1, 2, (), "hello");
+    let y = x;
     println!("{:?}, {:?}", x, y);
 }
