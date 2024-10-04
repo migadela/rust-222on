87da6083d00544f9f66e9291d5dfa8eb875fd631
Зінченко Вікторія <147725988+migadela@users.noreply.github.com>
2024-10-04 19:46:15 +0300

    Update and rename src/052t11.rs to src/ch5/052t11.rs

--- src/ch5/052t11.rs
+++ src/ch5/052t11.rs
@@ -6,4 +6,5 @@ fn main() {
 
     // Add one line below to make a compiler error: cannot borrow `s` as mutable more than once at a time
     // You can't use r1 and r2 at the same time
+    r1.push_str("world");
 }
