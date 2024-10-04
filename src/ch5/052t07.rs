98d06667ba0f26913594b414b4add6c180cec1f7
Зінченко Вікторія <147725988+migadela@users.noreply.github.com>
2024-10-04 19:30:53 +0300

    Update and rename src/052t07.rs to src/ch5/052t07.rs

--- src/ch5/052t07.rs
+++ src/ch5/052t07.rs
@@ -3,10 +3,14 @@
 fn main() {
     let mut s = String::from("hello");
 
-    let r1 = &mut s;
-    let r2 = &mut s;
+    {
+        let r1 = &mut s; 
+        println!("{}", r1); 
+    } 
 
-    println!("{}, {}", r1, r2);
+    let r2 = &s; 
+
+    println!("{}", r2); 
 
     println!("Success!");
 }
