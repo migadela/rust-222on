ba3929465643a6891378f5a326ae8a51ea6b2208
Зінченко Вікторія <147725988+migadela@users.noreply.github.com>
2024-10-04 19:39:33 +0300

    Update and rename src/052t10.rs to src/ch5/052t10.rs

--- src/ch5/052t10.rs
+++ src/ch5/052t10.rs
@@ -5,8 +5,8 @@ fn main() {
 
     let r1 = &mut s;
     r1.push_str("world");
-    let r2 = &mut s;
-    r2.push_str("!");
+    //let r2 = &mut s;
+    //r2.push_str("!");
     
     println!("{}",r1);
 }
