9539965497a63305bd4994a3322bc011d69a64cc
Зінченко Вікторія <147725988+migadela@users.noreply.github.com>
2024-10-08 23:09:13 +0200

    Update 066t03.rs

--- src/ch6/066t03.rs
+++ src/ch6/066t03.rs
@@ -8,13 +8,11 @@ enum Message {
 }
 
 fn main() {
-    let msg = Message::Move{x: 1, y: 2};
+    let msg = Message::Move{x: 1, y: 1};
 
-    if let Message::Move{__} = msg {
+    if let Message::Move{x: a, y: b} = msg {
         assert_eq!(a, b);
     } else {
         panic!("NEVER LET THIS RUN！");
     }
-
-    println!("Success!");
 } 
