e484238924aa96234ecf3d2ef0bdad6037baba63
Зінченко Вікторія <147725988+migadela@users.noreply.github.com>
2024-10-08 23:08:27 +0200

    Update 066t01.rs

--- src/ch6/066t01.rs
+++ src/ch6/066t01.rs
@@ -1,5 +1,5 @@
 
-// Fix the errors
+// Fix the error
 enum Number {
     Zero,
     One,
@@ -14,16 +14,14 @@ enum Number1 {
 
 // C-like enum
 enum Number2 {
-    Zero = 0.0,
-    One = 1.0,
-    Two = 2.0,
+    Zero = 0,
+    One = 1,
+    Two = 2,
 }
 
 
 fn main() {
-    // An enum variant can be converted to a integer by `as`
-    assert_eq!(Number::One, Number1::One);
-    assert_eq!(Number1::One, Number2::One);
-
-    println!("Success!");
+    // a enum variant can be converted to a integer by `as`
+    assert_eq!(Number::One as u8, Number1::One as u8);
+    assert_eq!(Number1::One as u8, Number2::One as u8);
 } 
