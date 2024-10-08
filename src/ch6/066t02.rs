aefb67ff30f7f14e2b4b0e78895bc76524213dbc
Зінченко Вікторія <147725988+migadela@users.noreply.github.com>
2024-10-08 23:08:50 +0200

    Update 066t02.rs

--- src/ch6/066t02.rs
+++ src/ch6/066t02.rs
@@ -8,8 +8,6 @@ enum Message {
 }
 
 fn main() {
-    let msg1 = Message::Move{__}; // Instantiating with x = 1, y = 2 
-    let msg2 = Message::Write(__); // Instantiating with "hello, world!"
-
-    println!("Success!");
+    let msg1 = Message::Move{x: 1, y: 2}; // instantiating with x = 1, y = 2 
+    let msg2 = Message::Write(String::from("hello, world")); // instantiating with "hello, world!"
 } 
