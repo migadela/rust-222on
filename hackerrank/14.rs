efa3758f45f70355abad5c32f52ce04cd0276af2
Зінченко Вікторія <147725988+migadela@users.noreply.github.com>
2024-10-27 21:41:02 +0200

    14.rs

---
+++ hackerrank/14.rs
@@ -0,0 +1,60 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'breakingRecords' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY scores as parameter.
+ */
+
+fn breakingRecords(scores: &[i32]) -> Vec<i32> {
+    if scores.is_empty() {
+        return vec![0, 0];
+    }
+
+    let mut highest = scores[0];
+    let mut lowest = scores[0];
+    let mut high_count = 0;
+    let mut low_count = 0;
+
+    for &score in scores.iter().skip(1) {
+        if score > highest {
+            highest = score;
+            high_count += 1;
+        } else if score < lowest {
+            lowest = score;
+            low_count += 1;
+        }
+    }
+
+    vec![high_count, low_count]
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let n = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    let scores: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let result = breakingRecords(&scores);
+
+    for i in 0..result.len() {
+        write!(&mut fptr, "{}", result[i]).ok();
+
+        if i != result.len() - 1 {
+            write!(&mut fptr, " ").ok();
+        }
+    }
+
+    writeln!(&mut fptr).ok();
+}
