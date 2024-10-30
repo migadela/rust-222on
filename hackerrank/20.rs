c335344bf19628388479657317dff5a059795d62
Зінченко Вікторія <147725988+migadela@users.noreply.github.com>
2024-10-30 21:46:14 +0200

    20.rs

---
+++ hackerrank/20.rs
@@ -0,0 +1,34 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+fn pageCount(n: i32, p: i32) -> i32 {
+    let front_turns = p / 2;
+    let back_turns = (n / 2) - (p / 2);
+    front_turns.min(back_turns)
+}
+
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let n = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    let p = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    let result = pageCount(n, p);
+
+    writeln!(&mut fptr, "{}", result).ok();
+}
